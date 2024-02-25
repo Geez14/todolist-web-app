21347fe2cea50154c61c3352eb354afcc2755285
aster14 <91541128+Geez14@users.noreply.github.com>
2024-02-25 13:09:35 +0530

    Add new response in views
    * Add new response header and add redirection

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    todos = Todo.objects.all()
+    if request.method == "POST" and request.POST["title"] != "":
+        new_todo = Todo(title=request.POST["title"])
+        new_todo.save()
+        return redirect("/")
+    return render(request, "index.html", {"todos": todos})
