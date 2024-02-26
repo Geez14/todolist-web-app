00d7b597c0988cfb109dea5cc45b819420ca4909
Mxtylish <91541128+Geez14@users.noreply.github.com>
2024-02-27 02:43:31 +0530

    Add Feature
    * Add Emoji Feature, user can now select emoji according to their mood
    * Changes and Bug fix in Controller and Views

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("delete/<str:pk>", views.delete, name="delete"),
+    path("description/<str:pk>", views.description, name="description"),
 ]
